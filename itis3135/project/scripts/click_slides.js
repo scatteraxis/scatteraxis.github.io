dd597f1d3b549da922193c78b1b053026ef3a2a6
scatteraxis <binchball2222@gmail.com>
2024-12-05 23:55:36 -0500

    fixes: fixed js code

--- itis3135/project/scripts/click_slides.js
+++ itis3135/project/scripts/click_slides.js
@@ -6,18 +6,14 @@ document.addEventListener("DOMContentLoaded", function() {
         let currentIndex = 0;
         
         function showSlide(index) {
-            // Hide all images
             slides.forEach((slide) => slide.style.display = 'none');
-            // Show the current image
             slides[index].style.display = 'block';
         }
         
-        // Initialize the first slide
         showSlide(currentIndex);
         
-        // Click handler for navigating through images
         slideshow.addEventListener('click', function() {
-            currentIndex = (currentIndex + 1) % slides.length;  // Loop back to the first image after the last one
+            currentIndex = (currentIndex + 1) % slides.length; 
             showSlide(currentIndex);
         });
     });
